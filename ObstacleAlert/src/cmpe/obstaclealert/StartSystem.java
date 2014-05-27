8951507b58e2d0c908f7d502b7b5c213be9202dd
burcincamci <burcincamci@095008b8-373d-f04c-16d9-7de5d7b5931f>
2014-05-27 10:01:38 +0000

    direction, sound notification

--- ObstacleAlert/src/cmpe/obstaclealert/StartSystem.java
+++ ObstacleAlert/src/cmpe/obstaclealert/StartSystem.java
@@ -6,6 +6,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.http.HttpResponse;
@@ -14,15 +16,21 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+
+
 import com.google.gson.Gson;
 
 import cmpe.obstaclealert.BackgroundService;
 import cmpe.obstaclealert.model.Obstacle;
 import cmpe.obstaclealert.model.ObstacleHolder;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.MediaScannerConnection;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -31,11 +39,13 @@ import android.provider.Settings;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+
 import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
@@ -46,16 +56,21 @@ public class StartSystem extends Activity implements LocationListener{
 	public ObstacleHolder obstacles;
 	public Obstacle[] allObstacles;
 
+	double coverage = 100;
+
 	private int i = 0;
 	private TextView latitudeText;
 	private TextView longitudeText;
+	private TextView directionText;
+	private TextView obsText;
 	private TextView otherText;
 
 	File dir;
 	File file;
 	FileOutputStream out;
 
-	private boolean canGetLocation = false;
+	private boolean canGetLocationNetwork = false;
+	private boolean canGetLocationGPS = false;
 	private LocationManager locationManager;
 	private static double latitude = 0;
 	private static double longitude = 0;
@@ -65,6 +80,14 @@ public class StartSystem extends Activity implements LocationListener{
 	private static final long MINIMUM_UPDATE_TIME = 0;
 	private static final float MINIMUM_UPDATE_DISTANCE = 0.0f;
 
+	private static double fromLatitude = 0.0;
+	private static double toLatitude = 0.0;
+	private static double fromLongitude = 0.0;
+	private static double toLongitude = 0.0;
+	private static Location fromLocation = null;
+	private static Location toLocation = null;
+
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -73,6 +96,8 @@ public class StartSystem extends Activity implements LocationListener{
 
 		latitudeText = (TextView) findViewById(R.id.latitudeText);
 		longitudeText = (TextView) findViewById(R.id.longitudeText);
+		directionText = (TextView) findViewById(R.id.directionText);
+		obsText = (TextView) findViewById(R.id.obsText);
 		otherText = (TextView) findViewById(R.id.otherText);
 
 		registerReceiver(powerButtonReceiver, powerButtonPressedIntentFilter());
@@ -88,21 +113,80 @@ public class StartSystem extends Activity implements LocationListener{
 		}
 
 		getFirstLocation();
-		if(!canGetLocation)
-			showSettingsAlert();
-		if(canGetLocation) {
+		if(!canGetLocationGPS)
+			showSettingsAlertGPS();
+		if(!canGetLocationNetwork)
+			showSettingsAlertNetwork();
+
+		if(canGetLocationGPS || canGetLocationNetwork) {
 			Location firstLoc = getFirstLocation();
 			double firstLong = firstLoc.getLongitude();
 			double firstLat = firstLoc.getLatitude();
 			loc = firstLoc;
 			longitude = firstLong;
 			latitude = firstLat;
-					
-			updateView(String.valueOf(latitude), String.valueOf(longitude));
-			getObstacles(longitude, latitude);
+			fromLocation = loc ;
+			fromLongitude = longitude;
+			fromLatitude = latitude;
+			toLocation = loc ;
+			toLongitude = longitude;
+			toLatitude = latitude;
+			double degree = fromLocation.bearingTo(toLocation);
+			String obs_type = "";
+			getObstacles(toLongitude, toLatitude);
+			Obstacle found = findObstacles(toLocation,degree);
+			if(found != null) {
+
+				showDialog(found);
+				obs_type = found.getType() ;
+			}
+
+			updateView(String.valueOf(toLatitude), String.valueOf(toLongitude), String.valueOf(degree), obs_type);
+
 		}
 
 	}
+	@SuppressWarnings("deprecation")
+	public void showDialog(Obstacle found){
+
+		AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
+		Log.i("am.isWiredHeadsetOn()", am.isWiredHeadsetOn()+"");
+		if(am.isWiredHeadsetOn()) {
+			MediaPlayer mp = MediaPlayer.create(this, R.raw.ding);
+
+			mp.start();
+		}
+
+		Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		// Vibrate for 500 milliseconds
+		v.vibrate(500);
+
+		String type = found.getType() ;
+
+
+		if(type.equals("stairs")) {
+			Toast.makeText(getApplicationContext(), "Be careful! There are stairs!",Toast.LENGTH_LONG).show();
+
+		}
+		else if(type.equals("sign")) {
+			Toast.makeText(getApplicationContext(), "Be careful! There is a traffic sign!",Toast.LENGTH_LONG).show();
+
+		}
+		else if(type.equals("hole")) {
+			Toast.makeText(getApplicationContext(), "Be careful! There is a hole!",Toast.LENGTH_LONG).show();
+
+		}
+		else if(type.equals("tree")) {
+			Toast.makeText(getApplicationContext(), "Be careful! There is a tree!",Toast.LENGTH_LONG).show();
+
+		}
+		else {
+			Toast.makeText(getApplicationContext(), "Be careful! There is an obstacle!",Toast.LENGTH_LONG).show();
+
+		}
+
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -135,10 +219,10 @@ public class StartSystem extends Activity implements LocationListener{
 
 	}
 
-	private void updateView(String latitude, String longitude){
+	private void updateView(String latitude, String longitude, String degree, String obs_type){
 		i++;
 		try {
-			out.write((longitude + "\t" + latitude +  "\n").getBytes());
+			out.write((longitude + "\t" + latitude + "\t" + degree +  "\n").getBytes());
 			out.flush();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -146,10 +230,12 @@ public class StartSystem extends Activity implements LocationListener{
 		// update all data in the UI
 		latitudeText.setText(latitude);
 		longitudeText.setText(longitude);
+		directionText.setText(degree);
+		obsText.setText(obs_type);
 		otherText.setText(i+"");
 	}
 	private static IntentFilter powerButtonPressedIntentFilter() {
-		final IntentFilter intentFilter2 = new IntentFilter(Intent.ACTION_SCREEN_ON);		
+		final IntentFilter intentFilter2 = new IntentFilter(Intent.ACTION_SCREEN_ON);    
 		intentFilter2.addAction(Intent.ACTION_SCREEN_OFF);
 		return intentFilter2;
 	}
@@ -172,15 +258,40 @@ public class StartSystem extends Activity implements LocationListener{
 	public void stopUsingGPS(){
 		if(locationManager != null){
 			locationManager.removeUpdates(StartSystem.this);
-			locationManager = null;		
+			locationManager = null;    
 		}       
 	}
 
-	public void showSettingsAlert(){
+	public void showSettingsAlertNetwork() 
+	{
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Network Setting");
+		builder.setMessage("Internet is not enabled. Do you want to go to settings menu?");
+		builder.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) 
+			{
+				getFirstLocation();
+				Intent intent = new Intent(Settings.ACTION_SETTINGS);
+				startActivity(intent);
+			}
+		});
+
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() 
+		{
+			public void onClick(DialogInterface dialog, int which) 
+			{
+				dialog.cancel();
+			}
+		});
+		builder.show();
+	}
+	public void showSettingsAlertGPS(){
+
 		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
 
 		// Setting Dialog Title
-		alertDialog.setTitle("GPS is settings");
+		alertDialog.setTitle("GPS Settings");
 
 		// Setting Dialog Message
 		alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
@@ -189,6 +300,7 @@ public class StartSystem extends Activity implements LocationListener{
 		// On pressing Settings button
 		alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog,int which) {
+				getFirstLocation();
 				Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
 				startActivity(intent);
 			}
@@ -204,13 +316,63 @@ public class StartSystem extends Activity implements LocationListener{
 		// Showing Alert Message
 		alertDialog.show();
 	}
+
 	@Override
 	public void onLocationChanged(Location location) {
 		loc = location;
 		latitude = location.getLatitude();
 		longitude = location.getLongitude();
-		updateView(String.valueOf(latitude), String.valueOf(longitude));
-		getObstacles(longitude, latitude);
+		fromLocation = toLocation ;
+		fromLongitude = toLongitude;
+		fromLatitude = toLatitude;
+		toLocation = loc ;
+		toLongitude = longitude;
+		toLatitude = latitude;
+		double degree = fromLocation.bearingTo(toLocation);
+		String obs_type = "";
+		getObstacles(toLongitude, toLatitude);
+		Obstacle found = findObstacles(toLocation,degree);
+		if(found != null) {
+
+			showDialog(found);
+			obs_type = found.getType() ;
+		}
+
+		updateView(String.valueOf(toLatitude), String.valueOf(toLongitude), String.valueOf(degree), obs_type);
+
+
+	}
+
+	public Obstacle findObstacles(Location location, double degree){
+		ArrayList<Obstacle> foundObstacles = new ArrayList<Obstacle>();
+		ArrayList<Double> foundDistances = new ArrayList<Double>();
+		int obsCount = allObstacles.length;
+		if(obsCount != 0) {
+
+			for(Obstacle obs : allObstacles) {
+
+				double temp_long = obs.getLongitude();
+				double temp_lat = obs.getLatitude();
+				Location temp_loc = location;
+				temp_loc.setLatitude(temp_lat);
+				temp_loc.setLongitude(temp_long);
+				double temp_degree = location.bearingTo(temp_loc);		
+				if( degree-coverage <= temp_degree && degree + coverage >= temp_degree) {
+					foundObstacles.add(obs);
+					double temp_dist = location.distanceTo(temp_loc);
+					foundDistances.add(temp_dist);		
+				}
+				if(foundObstacles.isEmpty()) {
+					return null;
+				}
+				else {
+					int minIndex = foundDistances.indexOf(Collections.min(foundDistances));
+					Obstacle returned = foundObstacles.get(minIndex);
+					return returned;
+				}
+			}
+		}
+		return null;
 	}
 
 	private void getObstacles (double longitude, double latitude) {
@@ -218,55 +380,45 @@ public class StartSystem extends Activity implements LocationListener{
 		MyAsyncTask task = new MyAsyncTask();
 		task.execute(Double.toString(longitude), Double.toString(latitude));
 		try {
-            String result = task.get();
-            Gson gson = new Gson();
+			String result = task.get();
+			Gson gson = new Gson();
 			obstacles = gson.fromJson(result, ObstacleHolder.class);
 			allObstacles = obstacles.getObstacles();
-			int obsCount = allObstacles.length;
-			Log.i("obstacle count", "" + obsCount);
-			Toast.makeText(getApplicationContext(), "obstacle count: " + obsCount,
-					Toast.LENGTH_LONG).show();
-			if(obsCount>0){
-				Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-				v.vibrate(500);
-			}
-				
-            
 		} catch (InterruptedException e) {
-            e.printStackTrace();
+			e.printStackTrace();
 		} catch (ExecutionException e) {
-            e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
-	
+
 	private StringBuilder inputStreamToString(InputStream is) {
-        String line = "";
-        StringBuilder total = new StringBuilder();
-        // Wrap a BufferedReader around the InputStream
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-        // Read response until the end
-        try {
-                while ((line = rd.readLine()) != null) { 
-                        total.append(line); 
-                }
-        } catch (IOException e) {
-                e.printStackTrace();
-        }
-        // Return full string
-        return total;
+		String line = "";
+		StringBuilder total = new StringBuilder();
+		// Wrap a BufferedReader around the InputStream
+		BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+		// Read response until the end
+		try {
+			while ((line = rd.readLine()) != null) { 
+				total.append(line); 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// Return full string
+		return total;
 	}
-	
+
 	public class MyAsyncTask extends AsyncTask<String, String, String> {
-		
+
 		int status = 0;
-		
+
 		@Override
 		protected String doInBackground(String... params) {
 			String mLongitude = params[0];
 			String mLatitude = params[1];
 			HttpClient httpclient = new DefaultHttpClient();
-			//HttpPost httppost = new HttpPost("http://79.123.176.62:8080/ObstacleAlert/GetObstaclesByLocation/" + mLongitude + "/" + mLatitude);
-			HttpPost httppost = new HttpPost("http://192.168.1.126:8080/ObstacleAlert/GetObstaclesByLocation/" + mLongitude + "/" + mLatitude);
+			HttpPost httppost = new HttpPost("http://79.123.176.83:8080/ObstacleAlert/GetObstaclesByLocation/" + mLongitude + "/" + mLatitude);
+			//HttpPost httppost = new HttpPost("http://192.168.1.126:8080/ObstacleAlert/GetObstaclesByLocation/" + mLongitude + "/" + mLatitude);
 			String result = "";
 			try {
 				// Add your data
@@ -285,17 +437,17 @@ public class StartSystem extends Activity implements LocationListener{
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			System.out.println("obstacles: "	+ result);
-			
+
+			System.out.println("obstacles: "  + result);
+
 			return result;
 		}
 
 		protected void onPostExecute(String result) {
 
 			if (status == 200) {
-//				Toast.makeText(getApplicationContext(), "command sent",
-//						Toast.LENGTH_LONG).show();
+				//        Toast.makeText(getApplicationContext(), "command sent",
+				//            Toast.LENGTH_LONG).show();
 			} else {
 				Toast.makeText(getApplicationContext(), "command could not sent",
 						Toast.LENGTH_LONG).show();
@@ -329,8 +481,13 @@ public class StartSystem extends Activity implements LocationListener{
 
 			if (!gpsEnabled && !isNetworkEnabled) {
 				// no network provider is enabled
-			} else {
-				canGetLocation = true;
+			}else if (!gpsEnabled && isNetworkEnabled) 
+				canGetLocationNetwork = true;
+			else if (gpsEnabled && !isNetworkEnabled)
+				canGetLocationGPS = true;
+			else {
+				canGetLocationNetwork = true;
+				canGetLocationGPS = true;
 				// First get location from Network Provider
 				if (isNetworkEnabled) {
 					locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,MINIMUM_UPDATE_TIME,MINIMUM_UPDATE_DISTANCE, this);
