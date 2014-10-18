b4b0c2db1331b8da94b741be00edc672b85c638b
burcincamci <burcincamci@095008b8-373d-f04c-16d9-7de5d7b5931f>
2014-10-18 09:35:23 +0000

    android_project_deneme

--- trunk/ObstacleAlert/src/cmpe/obstaclealert/model/Obstacle.java
+++ trunk/ObstacleAlert/src/cmpe/obstaclealert/model/Obstacle.java
@@ -3,37 +3,15 @@ package cmpe.obstaclealert.model;
 import com.google.gson.annotations.SerializedName;
 
 public class Obstacle {
-	@SerializedName("longitude")
-	private double longitude;
+	@SerializedName("name")
+	private String name;
 	
-	@SerializedName("latitude")
-	private double latitude;
-	
-	@SerializedName("type")
-	private String type;
-	
-	public double getLongitude() {
-		return longitude;
+	public String getName() {
+		return name;
 	}
 
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
-
-	public double getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
